cf4eda05b955193e9019ad96e27429af9432012d
Mohamed-AhmedThe <mohamedahmedayoub4@gmail.com>
2026-05-15 18:37:56 +0300

    the init was commented for testing

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,20 +1,20 @@
-# """
-# Agents Package
-# Contains all specialized AI units for the AgriBot framework.
-# All agents must inherit from BaseAgent.
-# """
+"""
+Agents Package
+Contains all specialized AI units for the AgriBot framework.
+All agents must inherit from BaseAgent.
+"""
 
-# from .base import BaseAgent
-# from .soil import SoilIntelligenceAgent, AgronomyStrategyAgent
-# from .weather import MicroClimateAgent
-# from .vision import CropPathologyAgent
+from .base import BaseAgent
+from .soil import SoilIntelligenceAgent, AgronomyStrategyAgent
+from .weather import MicroClimateAgent
+from .vision import CropPathologyAgent
 
-# # The __all__ list tells Python exactly what is allowed to be exported 
-# # when someone runs `from agents import *`
-# __all__ = [
-#     "BaseAgent",
-#     "SoilIntelligenceAgent",
-#     "AgronomyStrategyAgent",
-#     "MicroClimateAgent",
-#     "CropPathologyAgent"
-# ]
+# The __all__ list tells Python exactly what is allowed to be exported 
+# when someone runs `from agents import *`
+__all__ = [
+    "BaseAgent",
+    "SoilIntelligenceAgent",
+    "AgronomyStrategyAgent",
+    "MicroClimateAgent",
+    "CropPathologyAgent"
+]
